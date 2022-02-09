044fcdf7ac8e2ce15fd0a3b196793870d68b0660
hipster4020 <hipster4020@gmail.com>
2022-02-09 09:16:52 +0900

    model, trainer 수정

--- src/train.py
+++ src/train.py
@@ -30,38 +30,38 @@ def main(cfg):
     print(train_dataset[0])
     print(eval_dataset)
 
-    # args = TrainingArguments(
-    #     num_train_epochs=trainings["epochs"],
-    #     per_device_train_batch_size=trainings["train_batch_size"],
-    #     per_device_eval_batch_size=trainings["eval_batch_size"],
-    #     learning_rate=trainings["learning_rate"],
-    #     adam_beta1=trainings["adam_beta1"],
-    #     adam_beta2=trainings["adam_beta2"],
-    #     adam_epsilon=trainings["adam_epsilon"],
-    #     warmup_steps=trainings["warmup_steps"],
-    #     logging_dir=trainings["logging_dir"],
-    #     logging_steps=trainings["logging_steps"],
-    #     seed=trainings["seed"],
-    #     output_dir=etc["output_dir"],
-    # )
+    args = TrainingArguments(
+        num_train_epochs=cfg.TRAININGS.epochs,
+        per_device_train_batch_size=cfg.TRAININGS.train_batch_size,
+        per_device_eval_batch_size=cfg.TRAININGS.eval_batch_size,
+        learning_rate=cfg.TRAININGS.learning_rate,
+        adam_beta1=cfg.TRAININGS.adam_beta1,
+        adam_beta2=cfg.TRAININGS.adam_beta2,
+        adam_epsilon=cfg.TRAININGS.adam_epsilon,
+        warmup_steps=cfg.TRAININGS.warmup_steps,
+        logging_dir=cfg.TRAININGS.logging_dir,
+        logging_steps=cfg.TRAININGS.logging_steps,
+        seed=cfg.TRAININGS.seed,
+        output_dir=cfg.PATH.output_dir,
+    )
 
-    # pretrained_model_config = BertConfig.from_pretrained(
-    #     models["pretrained_model_name"],
-    #     num_labels=models["num_labels"],
-    # )
+    pretrained_model_config = BertConfig.from_pretrained(
+        cfg.MODEL.pretrained_model_name,
+        num_labels=cfg.MODEL.num_labels,
+    )
 
-    # model = BertForSequenceClassification.from_pretrained(
-    #     models["pretrained_model_name"],
-    #     config=pretrained_model_config,
-    # )
+    model = BertForSequenceClassification.from_pretrained(
+        cfg.MODEL.pretrained_model_name,
+        config=pretrained_model_config,
+    )
 
-    # trainer = Trainer(
-    #     model,
-    #     args,
-    #     train_dataset,
-    #     nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0, 3.0])),
-    #     eval_dataset=eval_dataset,
-    # )
+    trainer = Trainer(
+        model,
+        args,
+        train_dataset,
+        nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0, 3.0])),
+        eval_dataset=eval_dataset,
+    )
 
 
 if __name__ == "__main__":
